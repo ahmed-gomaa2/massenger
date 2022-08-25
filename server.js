64e84f7f1a5e929f1265fd62803f1f471f576d48
Ahmed_Gomaa <45296839+ahmed-gomaa2@users.noreply.github.com>
2022-08-25 21:26:54 +0200

    dflakjfdlakjf

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: '*'
+    origin: '*',
+    methods: ['GET', 'POST']
 }));
 
 app.use((req, res, next) => {
