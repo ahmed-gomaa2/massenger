4823a29968fe42410748df0b4e253bb6d97dec97
Ahmed_Gomaa <45296839+ahmed-gomaa2@users.noreply.github.com>
2022-08-25 21:13:54 +0200

    afldajfalkjfa;

--- server.js
+++ server.js
@@ -43,6 +43,12 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accept");
+    next();
+})
 // app.use('/files', express.static('./files'));
 
 // //default options
