8e1dbc89d2a3ba0cc4db3bffbf2c34dbdf69abbb
Ahmed_Gomaa <45296839+ahmed-gomaa2@users.noreply.github.com>
2022-08-27 01:41:18 +0200

    fdlakfjlakj

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,12 @@ import {MAKE_ROOM_UNSEEN} from "./store/actions/actionTypes";
 import Spinner from "./components/UI/Spinner/Spinner";
 
 
-const socket = io.connect('http://localhost:5000', (socket) => {
+const socket = io.connect('http://localhost:5000',{
+    withCredentials: true,
+    extraHeaders: {
+        "my-custom-header": "abcd"
+    }
+}, (socket) => {
   console.log(socket.id);
 });
 
