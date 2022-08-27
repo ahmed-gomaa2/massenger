dfec8426d2dc51d6dac390d417b2675e8fcfb70f
Ahmed_Gomaa <45296839+ahmed-gomaa2@users.noreply.github.com>
2022-08-27 03:14:29 +0200

    lfkjdaflkjakfj

--- client/src/components/UserRooms/UserRoom/UserRoom.js
+++ client/src/components/UserRooms/UserRoom/UserRoom.js
@@ -7,9 +7,15 @@ import {sidebarToggle} from "../../../store/actions/users";
 const UserRoom = props => {
     return (
         <div onClick={props.sidebarToggle} data-room-id={props.room.id} className={'UserRoom'}>
-            <NavLink to={`/${props.room.id}`}><span>{props.user?.username}</span> {!props.room.seen ? <span className={'UserRoom__unseen'}><i className="fa-solid fa-eye-slash"></i></span> : null}</NavLink>
+            <NavLink to={`/${props.rooms.filter(r => r.id === props.room.id)[0].id}`}><span>{props.user?.username}</span> {!props.rooms.filter(r => r.id === props.room.id)[0].seen ? <span className={'UserRoom__unseen'}><i className="fa-solid fa-eye-slash"></i></span> : null}</NavLink>
         </div>
     );
 };
+
+const mapStateToProps = state => {
+    return {
+        rooms: state.rooms.rooms
+    }
+}
 
 export default connect(null, {sidebarToggle}) (UserRoom);
