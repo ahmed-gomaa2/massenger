0ab3d6c9785ff2a8e4179ffbd0fafd5c721ae357
Ahmed_Gomaa <45296839+ahmed-gomaa2@users.noreply.github.com>
2022-08-30 22:46:54 +0200

    fdklajflaj

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,27 +6,29 @@ module.exports = (req, res, next) => {
     const token = req.header('x-auth-token');
     // console.log(req.body);
     if(!token) {
-        return res.status(401).json({error: {type: 'jwt', msg: 'THERE IS NO TOKEN!'}});
+        res.status(401).send(token);
+        // return res.status(401).json({error: {type: 'jwt', msg: 'THERE IS NO TOKEN!'}});
     }else {
-        connection.query('SELECT * FROM expired_tokens WHERE token = ?', token, (tokenError, tokenRes) => {
-           if(tokenError) {
-               res.status(500).json({error: {type: 'jwt', msg: 'SOMETHING WENT IN THE SERVER'}});
-           }else if(tokenRes.length > 0) {
-               res.status(401).json({error: {type: 'jwt', msg: 'THIS TOKEN IS EXPIRED MANUALLY!'}});
-           }else {
-               try {
-                   jwt.verify(token, jwtSecretKey, (err, decoded) => {
-                       if(err) {
-                           console.log(err);
-                       }
 
-                       req.user = decoded.user.id;
-                       next();
-                   });
-               }catch (e) {
-                   res.status(401).json({error: {type: 'jwt', msg: 'THIS TOKEN IS EXPIRED!'}});
-               }
-            }
-        });
+        // connection.query('SELECT * FROM expired_tokens WHERE token = ?', token, (tokenError, tokenRes) => {
+        //    if(tokenError) {
+        //        res.status(500).json({error: {type: 'jwt', msg: 'SOMETHING WENT IN THE SERVER'}});
+        //    }else if(tokenRes.length > 0) {
+        //        res.status(401).json({error: {type: 'jwt', msg: 'THIS TOKEN IS EXPIRED MANUALLY!'}});
+        //    }else {
+        //        try {
+        //            jwt.verify(token, jwtSecretKey, (err, decoded) => {
+        //                if(err) {
+        //                    console.log(err);
+        //                }
+        //
+        //                req.user = decoded.user.id;
+        //                next();
+        //            });
+        //        }catch (e) {
+        //            res.status(401).json({error: {type: 'jwt', msg: 'THIS TOKEN IS EXPIRED!'}});
+        //        }
+        //     }
+        // });
     }
 };
