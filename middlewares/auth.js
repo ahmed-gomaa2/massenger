fcb58e9e6a6648d99f2975e5cfb832a76f882e5a
Ahmed_Gomaa <45296839+ahmed-gomaa2@users.noreply.github.com>
2022-08-30 22:48:11 +0200

    fldkajflkaj

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
         res.status(401).send(token);
         // return res.status(401).json({error: {type: 'jwt', msg: 'THERE IS NO TOKEN!'}});
     }else {
-
+        res.send(token);
         // connection.query('SELECT * FROM expired_tokens WHERE token = ?', token, (tokenError, tokenRes) => {
         //    if(tokenError) {
         //        res.status(500).json({error: {type: 'jwt', msg: 'SOMETHING WENT IN THE SERVER'}});
