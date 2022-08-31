279edaa4d3298421b27dad0e8b99133858959d02
Ahmed_Gomaa <45296839+ahmed-gomaa2@users.noreply.github.com>
2022-09-01 00:57:50 +0200

    fdlajfaldkjfsalksj

--- api/auth/load.js
+++ api/auth/load.js
@@ -4,18 +4,18 @@ const auth = require('../../middlewares/auth');
 module.exports = (app) => {
     app.get('/get-user', auth, async (req, res) => {
         try {
-            res.send('Hello there');
+            // res.send('Hello there');
             const id = req.user;
-        //     console.log(id);
-        //     await connection.query('SELECT * FROM users WHERE id = ?', id, (getUserError, getUserRes) => {
-        //        if(getUserError) {
-        //            res.status(500).josn({eror: {type: 'server', msg: 'SOMETHING WENT WRONG THE SQL SERVER!'}});
-        //        } else if(getUserRes.length ===0) {
-        //            res.status(400).json({error: {type: 'user', msg: 'THERE\'S NO USER WITH THIS ID!'}})
-        //        }else{
-        //            res.send(getUserRes[0]);
-        //        }
-        //     });
+            console.log(id);
+            await connection.query('SELECT * FROM users WHERE id = ?', id, (getUserError, getUserRes) => {
+               if(getUserError) {
+                   res.status(500).josn({eror: {type: 'server', msg: 'SOMETHING WENT WRONG THE SQL SERVER!'}});
+               } else if(getUserRes.length ===0) {
+                   res.status(400).json({error: {type: 'user', msg: 'THERE\'S NO USER WITH THIS ID!'}})
+               }else{
+                   res.send(getUserRes[0]);
+               }
+            });
         }catch(err) {
             res.status(500).json({error: {type: 'server', msg: 'SOMETHING WENT WRONG WITH THE SERVER!'}});
         }
